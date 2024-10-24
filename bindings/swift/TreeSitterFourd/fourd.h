052d47e0e32cdff1b2200a0b07d263db1eccd6ea
Kyle Kincer <kyle_kincer@sweetwater.com>
2024-10-24 16:09:26 -0400

    upgrade langauge version to 13

---
+++ bindings/swift/TreeSitterFourd/fourd.h
@@ -0,0 +1,16 @@
+#ifndef TREE_SITTER_FOURD_H_
+#define TREE_SITTER_FOURD_H_
+
+typedef struct TSLanguage TSLanguage;
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+const TSLanguage *tree_sitter_fourd(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // TREE_SITTER_FOURD_H_
